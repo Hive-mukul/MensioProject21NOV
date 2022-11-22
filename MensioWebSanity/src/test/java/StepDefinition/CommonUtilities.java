6195b7dc91b1641a305172284d3b3371677fea1d
Hive-mukul <mukul.kumar@thehive.ai>
2022-11-22 15:15:20 +0530

    6th commit with changed directory of screenshot folder

--- MensioWebSanity/src/test/java/StepDefinition/CommonUtilities.java
+++ MensioWebSanity/src/test/java/StepDefinition/CommonUtilities.java
@@ -18,20 +18,20 @@ import ru.yandex.qatools.ashot.AShot;
 import ru.yandex.qatools.ashot.Screenshot;
 
 public class CommonUtilities {
-	
+
 	static String DEV = "https://dev.mensio.com/login";
 	static String STG = "https://stg.mensio.com/login";
 	static String PROD = "https://mensio.com/login";
 	static String projectPath = System.getProperty("user.dir");
 	//System.out.println("Project Path is:"+projectPath);
 	public static WebDriver driver;
-	
-	
 
-	
+
+
+
 public static void Chrome()
 {
-	
+
 	System.setProperty("webdriver.chrome.driver",projectPath+"/src/test/resources/drivers/chromedriver");
 //	System.setProperty("webdriver.chrome.driver",projectPath+"/src/test/resources/drivers/chromedriver.exe");
 
@@ -40,7 +40,7 @@ public static void Chrome()
 	driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 	driver.manage().window().maximize();
 	driver.navigate().to(STG);
-    System.out.println("User is on the login screen");	
+    System.out.println("User is on the login screen");
 }
 
 public static void Home()
@@ -49,10 +49,10 @@ public static void Home()
 	driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/div[1]/div/a[1]")).click();
 }
 public static void Screenshot(String FolderName, String name) {
-	Screenshot screenshot = new AShot().takeScreenshot(driver); 
+	Screenshot screenshot = new AShot().takeScreenshot(driver);
     try {
-    
-		ImageIO.write(screenshot.getImage(), "png", new File(projectPath+"/target/Screenshots/"+FolderName+"/"+name+".png"));
+
+		ImageIO.write(screenshot.getImage(), "png", new File(projectPath+"/src/Screenshots/"+FolderName+"/"+name+".png"));
 
 	} catch (IOException e) {
 		// TODO Auto-generated catch block
